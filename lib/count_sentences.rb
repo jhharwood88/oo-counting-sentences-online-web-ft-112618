880a40e45abc925f7508a04c97b7a3b8f1c7d2fc
Jonathan Harwood <jhharwood88@gmail.com>
2018-12-07 21:20:45 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,8 +15,14 @@ class String
   end
 
   def count_sentences
+<<<<<<< HEAD
     
    self.squeeze("!").split(/[.!?]/).count
    #binding.pry
+=======
+    self.split(".").count
+    
+>>>>>>> 51d982e2dd073d5f78653834031e8046e6534fe2
   end
+  binding.pry
 end
